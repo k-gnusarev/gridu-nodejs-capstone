4adf252e2e2872ad8db4f2cfc4333102f0e703d3
Konstantin Gnusarev <kgnusarev@griddynamics.com>
2023-03-29 16:25:51 -0600

    setup database skeleton

--- index.mjs
+++ index.mjs
@@ -1,9 +1,15 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import Db from "./src/db/index.mjs"
+import bp from 'body-parser'
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
 
 app.use(cors())
+app.use(bp.urlencoded({extended: true}))
+app.use(bp.json())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -12,13 +18,16 @@ app.get('/', (req, res) => {
 app.post('/api/users', (req, res) => {
   console.log(req)
   console.log('pow')
+  res.writeHead(200)
+  res.end('ok')
 })
 app.get('/api/users')
 app.post('/api/users/:_id/exercises')
 app.get('/api/users/:_id/logs')
 
 
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+const db = new Db('test.js')
---
+++ src/db/index.mjs
@@ -0,0 +1,41 @@
+import {Database} from "sqlite-async";
+
+export default class Db {
+  constructor(name) {
+    this.name = name
+
+    Database.open(this.name)
+      .then(async db => {
+        this.instance = db;
+
+        await this.instance.run(`CREATE TABLE IF NOT EXISTS Users
+                        (_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username VARCHAR(100) NOT NULL);
+                      `);
+        await this.instance.run(`CREATE TABLE IF NOT EXISTS Exercises 
+                        (_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        description VARCHAR(100) NOT NULL,
+                        duration INTEGER NOT NULL,
+                        date INTEGER DEFAULT (STRFTIME('%s', 'now') * 1000),
+                        userId INTEGER NOT NULL,
+                        
+                        FOREIGN KEY (userId) REFERENCES Users(_id))
+                        `);
+        this.instance.run('PRAGMA foreign_keys = ON;');
+      })
+      .catch(e => console.error('Can not open database connection', e));
+  }
+
+  async run(query, ...args) {
+    return await this.instance.run(query, args);
+  }
+
+  async get(query, ...args) {
+    return await this.instance.get(query, args);
+  }
+
+  async all(query, ...args) {
+    return await this.instance.all(query, args);
+  }
+}
+
